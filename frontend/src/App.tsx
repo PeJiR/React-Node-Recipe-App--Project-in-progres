368fd8259d7bf71eb7cc3f513d83b80fd32f574a
pejir <pejir84@gmail.com>
2024-04-01 00:00:25 +0200

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
-import React from "react";
 import './App.css';
+import { useState } from 'react';
+import * as api from './api';
 
 const App = ()=>{
+  const [searchTerm, setSearchTerm]= useState("burgers");
+  const [recipes, setRecipes] = useState([])
+  const handleSearchSubmit = async()=>{
+    try {
+      const recipes = await api.searchRecipes(searchTerm, 1)
+      
+      
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
+
   return<div> Hello from recipe App!</div>
 };
 
