656ac6b760d47fc384699f476dd0ec0f3d4abe6c
pejir <pejir84@gmail.com>
2024-03-31 03:37:18 +0200

    update

---
+++ backend/src/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/api/recipe/search", async (req, res) => {
+  res.json({ message: "success" });
+});
+
+app.listen(5000, () => {
+  console.log("Server running on localhost:5000");
+});
+
+
+
+
+
+
