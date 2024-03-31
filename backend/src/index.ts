d809f68f4f760580277ee56b52b4a9d5e7dff18f
pejir <pejir84@gmail.com>
2024-03-31 21:35:59 +0200

    update

---
+++ backend/src/index.ts
@@ -0,0 +1,30 @@
+//Setup Your Backend Code
+
+import express from "express";
+import cors from "cors";
+import * as recipeAPI from './recipe-api'
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/api/recipe/search", async (req, res) => {
+  //Get http://localhost/api/recipes/search?searchTerm=burguers&page=1
+  const searchTerm =req.query.searchTerm as string;
+  const page =parseInt(req.query.page as string);
+  const results = recipeAPI.searchRecipes(searchTerm, page);
+
+  return res.json(results);
+});
+
+app.listen(5000, () => {
+  console.log("Server running on localhost:5000");
+});
+
+
+
+
+
+
+
